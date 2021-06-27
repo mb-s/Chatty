d2f71b2c1bc488aaf294eb995047bae2df5391ba
Mika <mika0089@dk.edum.org.il>
2021-06-27 18:14:58 +0300

    Make splash screen nicer

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,6 +11,30 @@ class SplashScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
+              transform: Matrix4.rotationZ(-6 * pi / 180)..translate(-10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Theme.of(context).backgroundColor,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 8,
+                    color: Colors.black26,
+                    offset: Offset(0, 2),
+                  )
+                ],
+              ),
+              child: Text(
+                'Chatty',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 40,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
             Text('Loading...'),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.05,
